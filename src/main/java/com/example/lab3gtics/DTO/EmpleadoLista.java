abe09fb98a4dd47b78690e30e38444f84dbb8a49
DiegoZuasnabar <a20190459@pucp.edu.pe>
2022-09-20 21:04:45 -0500

    JexitoEmployee

---
+++ src/main/java/com/example/lab3gtics/DTO/EmpleadoLista.java
@@ -0,0 +1,9 @@
+package com.example.lab3gtics.DTO;
+
+public interface EmpleadoLista {
+    String getNombre();
+    int getApellido();
+    String getPuesto();
+    String getDepartamento();
+    String getCiudad();
+}
