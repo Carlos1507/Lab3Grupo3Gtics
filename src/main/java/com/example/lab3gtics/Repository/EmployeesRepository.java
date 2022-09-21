c12ad4f054c35e699248c9692b58340bf44ada97
DiegoZuasnabar <a20190459@pucp.edu.pe>
2022-09-20 21:11:03 -0500

    JexitoEmployeev2

--- src/main/java/com/example/lab3gtics/Repository/EmployeesRepository.java
+++ src/main/java/com/example/lab3gtics/Repository/EmployeesRepository.java
@@ -10,6 +10,11 @@ import java.util.List;
 
 @Repository
 public interface EmployeesRepository extends JpaRepository<Employees,Integer> {
-    @Query(nativeQuery = true, value = "")
-    List<EmpleadoLista> buscarEmpleados(String a);
+    @Query(nativeQuery = true, value = "select e.first_name, e.last_name,j.job_title,d.department_name, l.city from employees e " +
+            "inner join departments d on e.department_id = d.department_id " +
+            "inner join locations l on d.location_id = l.location_id " +
+            "inner join jobs j on j.job_id=e.job_id " +
+            "where (e.first_name like %?1% or e.last_name like %?1% or " +
+            "       j.job_title like %?1% or d.department_name like %?1% or l.city like %?1%)")
+    List<EmpleadoLista> buscarEmpleados(String filtro);
 }
