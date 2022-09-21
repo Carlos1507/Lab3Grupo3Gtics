ee7c7e3c6ac7673eb26037bfe4644d9b354bcf76
david34Y <dgonzalesn@pucp.edu.pe>
2022-09-20 21:50:36 -0500

    Historial query 1

--- src/main/java/com/example/lab3gtics/Repository/HistorialRepository.java
+++ src/main/java/com/example/lab3gtics/Repository/HistorialRepository.java
@@ -2,7 +2,6 @@ package com.example.lab3gtics.Repository;
 
 import com.example.lab3gtics.DTO.Historial_lista;
 import com.example.lab3gtics.Entity.JobHistory;
-import com.example.lab3gtics.Entity.JobHistoryPK;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -11,7 +10,7 @@ import java.util.List;
 
 @Repository
 public interface HistorialRepository extends JpaRepository<JobHistory, Integer> {
-@Query(value = "select e.employee_id ,e.first_name, e.last_name, d.department_name, m.employee_id ,m.first_name, m.last_name, j.job_history_id, j.start_date, j.end_date from employees e, departments d, employees m, job_history j where e.department_id = d.department_id and e.manager_id=m.employee_id and e.hire_date = j.start_date and e.job_id = j.job_id\n" +
-        "order by j.start_date desc", nativeQuery = true)
-List<Historial_lista> obtenerHistorial();
+    @Query(value = "select e.employee_id ,e.first_name, e.last_name, d.department_name, m.employee_id ,m.first_name, m.last_name, j.job_history_id, j.start_date, j.end_date from employees e, departments d, employees m, job_history j where e.department_id = d.department_id and e.manager_id=m.employee_id and e.hire_date = j.start_date and e.job_id = j.job_id\n" +
+            "order by j.start_date desc", nativeQuery = true)
+    List<Historial_lista> obtenerHistorial();
 }
